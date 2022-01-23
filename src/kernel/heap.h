37cf646de9a689de50a5a9740a4e24a84cb89ea2
NickWalker1 <nickwalk9@gmail.com>
2022-01-23 17:43:30 +0000

    need to improve heap tests

--- src/kernel/heap.h
+++ src/kernel/heap.h
@@ -7,11 +7,18 @@
 
 typedef struct MemorySegmentHeader MemorySegmentHeader_t;
 
+#define segment_magic 0x87654321
+
 struct MemorySegmentHeader{
-    //implement here
+    bool free;
+    uint32_t size;
+    MemorySegmentHeader_t* next;
+    MemorySegmentHeader_t* previous;
+    uint32_t magic;
 };
 
 void intialiseHeap(void* base, void* limit);
 void *malloc(uint32_t size);
 void free(void* addr);
 void clear_heap(void* addr);
+void heap_dump();
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -4,7 +4,7 @@
 
 #include "heap.h"
 
-int NUMTESTS=5;
+int NUMTESTS=6;
 
 uint32_t* KHEAP_ADDR;
 uint32_t* KHEAP_ADDR_MAX;
@@ -86,8 +86,8 @@ void testReport(){
  * WARNING: Tests not entirely exhaustive.
  */
 int performTest(){
-	int mark=0b11111;
-	uint32_t test1,test2,test3,test4;
+	int mark=0b111111;
+	uint32_t test1,test2,test3,test4,test5;
 	uint32_t MSH = sizeof(MemorySegmentHeader_t);
 
 
@@ -152,6 +152,39 @@ int performTest(){
 	if(test4!=(test1+0x50+MSH)) mark=mark ^1<<4;
 	
 
+	//TEST 6: Free again
+
+	clear_heap(KHEAP_ADDR);
+	intialiseHeap(KHEAP_ADDR,KHEAP_ADDR_MAX);
+	test1=(uint32_t)malloc(0x50);
+	test2=(uint32_t)malloc(0x50);
+	test3=(uint32_t)malloc(0x50);
+	malloc(0x50);
+	free((void*)test3);
+	free((void*)test2);
+	test4= (uint32_t) malloc(0x70);
+	if(test4!=(test1+0x50+MSH)) mark=mark ^1<<4;
+	
+	heap_dump();
+
+
+	//TEST 7: Free again
+
+	clear_heap(KHEAP_ADDR);
+	intialiseHeap(KHEAP_ADDR,KHEAP_ADDR_MAX);
+
+	test1=(uint32_t)malloc(0x50);
+	test2=(uint32_t)malloc(0x50);
+	test3=(uint32_t)malloc(0x50);
+	test4=(uint32_t)malloc(0x50);
+	malloc(0x50);
+	free((void*)test2);
+	free((void*)test4);
+	free((void*)test3);
+	test5= (uint32_t) malloc(0x150+2*MSH);
+	free(test5);
+
+	heap_dump();
 
 	return mark;
 }
